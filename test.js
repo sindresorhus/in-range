54dd17554a40661ffbe402c1b64fd188d4043c62
Sindre Sorhus <sindresorhus@gmail.com>
2016-01-01 01:12:56 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,19 +1,18 @@
-'use strict';
-var test = require('ava');
-var fn = require('./');
+import test from 'ava';
+import m from './';
 
-test(function (t) {
-	t.assert(fn(0, 0));
-	t.assert(fn(1, 1));
-	t.assert(fn(5, 10));
-	t.assert(fn(1, 0, 1));
-	t.assert(fn(2, 0, 2));
-	t.assert(fn(5, 0, 10));
-	t.assert(fn(10, -Infinity, Infinity));
-	t.assert(fn(-10, -Infinity, Infinity));
-	t.assert(fn(-1, -10, 10));
-	t.assert(fn(1.5, 1.1, 1.7));
-	t.assert(fn(1.5, 1.1, 1.7));
-	t.assert(fn(5, 10, 1));
-	t.end();
+test(t => {
+	t.true(m(0, 0));
+	t.true(m(1, 1));
+	t.true(m(5, 10));
+	t.true(m(1, 0, 1));
+	t.true(m(2, 0, 2));
+	t.true(m(5, 0, 10));
+	t.true(m(10, -Infinity, Infinity));
+	t.true(m(-10, -Infinity, Infinity));
+	t.true(m(-1, -10, 10));
+	t.true(m(1.5, 1.1, 1.7));
+	t.true(m(1.5, 1.1, 1.7));
+	t.true(m(5, 10, 1));
+	t.false(m(3, 1, 2));
 });
